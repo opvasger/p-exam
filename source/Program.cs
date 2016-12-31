5917896e97b0e1bbabf2dea121bf70ec5929f623
opvasger <opvasger@gmail.com>
2016-12-31 16:28:10 +0100

    reverted to dotnet core

---
+++ source/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Program {
+    public class Index {
+        public static void Main (string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
