be96a29a6120aa926d4ba3e71e64bdc4e4e820dc
opvasger <opvasger@gmail.com>
2016-12-31 17:21:04 +0100

    fetch some json and print it

--- source/Program.cs
+++ source/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Net.Http;
 
-namespace Program {
-    public class Index {
-        public static void Main (string[] args) {
-            Console.WriteLine("Hello World!");
+namespace Program
+{
+    public class Index
+    {
+        public static void Main (string[] args)
+        {
+            new HttpClient()
+            .GetAsync("http://mtgjson.com/json/AllSetsArray-x.json")
+            .ContinueWith(async response => {
+                var result = await response;
+                var body = await result.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            })
+            .Wait();
         }
     }
 }
