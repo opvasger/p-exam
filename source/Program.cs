3cd74e4ed70d3f187dc61004a8c16a10f1be9d2d
opvasger <opvasger@gmail.com>
2016-12-31 17:23:17 +0100

    who needs namespaces anyhow

--- source/Program.cs
+++ source/Program.cs
@@ -1,20 +1,17 @@
 ﻿using System;
 using System.Net.Http;
 
-namespace Program
+public class Program
 {
-    public class Index
+    public static void Main (string[] args)
     {
-        public static void Main (string[] args)
-        {
-            new HttpClient()
-            .GetAsync("http://mtgjson.com/json/AllSetsArray-x.json")
-            .ContinueWith(async response => {
-                var result = await response;
-                var body = await result.Content.ReadAsStringAsync();
-                Console.WriteLine(body);
-            })
-            .Wait();
-        }
+        new HttpClient()
+        .GetAsync("http://mtgjson.com/json/AllSetsArray-x.json")
+        .ContinueWith(async response => {
+            var result = await response;
+            var body = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        })
+        .Wait();
     }
 }
