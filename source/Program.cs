1bc386e5a46d010d922c3de5e9195df28adedeb4
w0ns88 <w0ns88>
2017-01-02 14:32:52 +0100

    extracted logic to methods, elapsed time on synchronous methods

--- source/Program.cs
+++ source/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -20,46 +21,62 @@ public class Program
             var setSerializer = new DataContractJsonSerializer(typeof(List<Model.Set>));
             var bodyStream = new MemoryStream(Encoding.UTF8.GetBytes(body));
             var sets = (List<Model.Set>)setSerializer.ReadObject(bodyStream);
+            
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            PrintLegendarySet(sets);
+            PrintRedCards(sets);
+            PrintMostReprinted(sets);
+            PrintMostPopularColorCombination(sets);
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
 
-            // Log the name of the set with most legendary cards
-            var mostLegendarySet = sets.Select(set => new {
-                SetName = set.Name,
-                LegendCount = set.Cards
-                    .Where(card =>
-                        card.SuperTypes != null
-                        &&  card.SuperTypes
-                            .Contains("Legendary"))
-                            .ToList()
-                            .Count
-            })
-            .OrderByDescending(set => set.LegendCount)
-            .FirstOrDefault();
+            Console.WriteLine();
+            Console.WriteLine("Elapsed Time: " + ts);
+            Console.WriteLine();
+        })
+        .Wait();
+    }
 
-            Console.WriteLine(
-                "{0} is the most legendary set with {1} legendary cards",
-                mostLegendarySet.SetName,
-                mostLegendarySet.LegendCount
-            );
+    public static void PrintMostPopularColorCombination(List<Model.Set> sets)
+    {
+        var mostPopularColorCombination =
+            sets
+                .SelectMany(set => set.Cards)
+                .Where(card => card.Colors != null && card.Colors.Count > 1)
+                .Aggregate(
+                    new Dictionary<string, int>(),
+                    (cardMap, card) =>
+                    {
+                        var key = String.Join(",", card.Colors);
 
-            // Log the count of all red cards
-            var redCards = sets
-            .SelectMany(set => set.Cards)
-            .Where(card => card.Colors != null && card.Colors.Contains("R"))
-            .ToList()
+                        if (cardMap.ContainsKey(key))
+                            cardMap[key] += 1;
+                        else
+                            cardMap.Add(key, 1);
+                        return cardMap;
+                    }
+                )
+                .OrderBy(card => card.Value)
+                .LastOrDefault();
 
-            Console.WriteLine(
-                "There is {0} red Magic cards",
-                redCards.Count
-            );
+        Console.WriteLine(
+            "{0} is the most popular Magic card color combination with {1} cards",
+            mostPopularColorCombination.Key,
+            mostPopularColorCombination.Value
+        );
+    }
 
-            // Log the name of the most reprinted card
-            var mostReprinted = sets
+    public static void PrintMostReprinted(List<Model.Set> sets)
+    {
+        var mostReprinted = sets
             .SelectMany(set => set.Cards)
             .Aggregate(
                 new Dictionary<string, int>(),
-                (cardMap, card) => {
+                (cardMap, card) =>
+                {
                     if (cardMap.ContainsKey(card.Name))
-                         cardMap[card.Name] = cardMap[card.Name] + 1;
+                        cardMap[card.Name] = cardMap[card.Name] + 1;
                     else
                         cardMap.Add(card.Name, 0);
                     return cardMap;
@@ -68,40 +85,46 @@ public class Program
             .OrderByDescending(card => card.Value)
             .FirstOrDefault();
 
-            Console.WriteLine(
-                "{0} is the most reprinted Magic card with {1} reprints",
-                mostReprinted.Key,
-                mostReprinted.Value
-            );
+        Console.WriteLine(
+            "{0} is the most reprinted Magic card with {1} reprints",
+            mostReprinted.Key,
+            mostReprinted.Value
+        );
+    }
 
+    public static void PrintLegendarySet(List<Model.Set> sets)
+    {
+        var mostLegendarySet = sets.Select(set => new
+            {
+                SetName = set.Name,
+                LegendCount = set.Cards
+                    .Where(card =>
+                        card.SuperTypes != null
+                        && card.SuperTypes
+                            .Contains("Legendary"))
+                    .ToList()
+                    .Count
+            })
+            .OrderByDescending(set => set.LegendCount)
+            .FirstOrDefault();
 
-            // Log the most popular combination of colors
-            var mostPopularColorCombination =
-            sets
+        Console.WriteLine(
+            "{0} are the most legendary set with {1} legendary cards",
+            mostLegendarySet.SetName,
+            mostLegendarySet.LegendCount
+        );
+    }
+
+    public static void PrintRedCards(List<Model.Set> sets)
+    {
+        var redCards = sets
             .SelectMany(set => set.Cards)
-            .Where(card => card.Colors != null && card.Colors.Count > 1)
-            .Aggregate(
-                new Dictionary<string, int>(),
-                (cardMap, card) => {
-                    var key = String.Join(",", card.Colors);
+            .Where(card => card.Colors != null && card.Colors.Contains("R"))
+            .ToList();
 
-                    if (cardMap.ContainsKey(key))
-                        cardMap[key] += 1;
-                    else
-                        cardMap.Add(key, 1);
-                    return cardMap;
-                }
-            )
-            .OrderBy(card => card.Value)
-            .LastOrDefault();
-
-            Console.WriteLine(
-                "{0} is the most popular Magic card color combination with {1} cards",
-                mostPopularColorCombination.Key,
-                mostPopularColorCombination.Value
-            );
-
-        })
-        .Wait();
+        Console.WriteLine(
+            "There is {0} red Magic cards",
+            redCards.Count
+        );
     }
 }
